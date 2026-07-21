e07b965ae077ffdf8d5ece0584ccf292d22f848f
dangngocdp <dangngoc.dp@gmail.com>
2026-07-21 21:21:28 +0700

    Create monitor.py

---
+++ monitor.py
@@ -0,0 +1,17 @@
+import requests
+import os
+
+BOT_TOKEN = os.environ["TELEGRAM_BOT_TOKEN"]
+CHAT_ID = os.environ["TELEGRAM_CHAT_ID"]
+
+message = "✅ Xin chào!\n\nBot Website Monitor đã chạy thành công trên GitHub."
+
+url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
+requests.post(
+    url,
+    json={
+        "chat_id": CHAT_ID,
+        "text": message
+    }
+)
